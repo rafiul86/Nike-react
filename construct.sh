eb5d46653bedecfabf6fb977a245c5bc3a213ba0
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-08-10 15:19:36 +0600

    bash scripts added

--- construct.sh
+++ construct.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read -t 5 -p "Enter your name: " name
-read -t 4 -p "Enter your age: " age
-read -t 5 -p "Enter your city: " city
+read -s -t 5 -p "Enter your name: " name
+read -s -t 4 -p "Enter your age: " age
+read -s -t 5 -p "Enter your city: " city
 
 echo "Your name is $name, age is $age, city is $city"
